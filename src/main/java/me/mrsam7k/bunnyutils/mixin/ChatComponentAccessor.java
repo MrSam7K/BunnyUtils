fad95632a4a897a74f54057223615e3a922dccc4
KingsMMA <63374258+KingsMMA@users.noreply.github.com>
2022-12-24 19:29:52 +1000

    Reformatted some code (feel free to revert)

--- src/main/java/me/mrsam7k/bunnyutils/mixin/ChatComponentAccessor.java
+++ src/main/java/me/mrsam7k/bunnyutils/mixin/ChatComponentAccessor.java
@@ -2,7 +2,6 @@ package me.mrsam7k.bunnyutils.mixin;
 
 import net.minecraft.client.GuiMessage;
 import net.minecraft.client.gui.components.ChatComponent;
-import net.minecraft.util.FormattedCharSequence;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
--- src/main/java/me/mrsam7k/bunnyutils/util/ScreenUtil.java
+++ src/main/java/me/mrsam7k/bunnyutils/util/ScreenUtil.java
@@ -4,12 +4,13 @@ public final class ScreenUtil {
 
     /**
      * Determines if the given mouse coordinates are hovering over a region
+     *
      * @param mouseX The mouse X coordinate
      * @param mouseY The mouse Y coordinate
-     * @param x1 The region's first X coordinate
-     * @param y1 The region's first Y coordinate
-     * @param x2 The region's second X coordinate
-     * @param y2 The region's second Y coordinate
+     * @param x1     The region's first X coordinate
+     * @param y1     The region's first Y coordinate
+     * @param x2     The region's second X coordinate
+     * @param y2     The region's second Y coordinate
      * @return If the given mouse coordinates are hovering over a region
      */
     public static boolean hovering(double mouseX, double mouseY, int x1, int y1, int x2, int y2) {
